f5a193250afb1670802b938dc1d27f9986f82263
Gaurav Upadhyay <gauravupadhyay.work@gmail.com>
2023-09-23 17:28:26 +0530

    nah
    2nd

---
+++ chatbot-sih/sidepanel/page.js
@@ -0,0 +1,15 @@
+
+
+function SidePanel() {
+  return (
+    <div>
+        <ul>
+            <li>
+                ABC
+            </li>
+        </ul>
+    </div>
+  )
+}
+
+export default SidePanel
